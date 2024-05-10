3193f67f7dad37228d9c502aba065afabeef1b2b
henriquegardini <henrique.gardini@hotmail.com>
2024-05-09 22:44:44 -0300

    feat: inclusao do caso negativo no teste CreateReviewUseCaseTest.

--- src/test/java/br/com/fiap/techChallenge3/usecase/review/CreateReviewUseCaseTest.java
+++ src/test/java/br/com/fiap/techChallenge3/usecase/review/CreateReviewUseCaseTest.java
@@ -2,6 +2,7 @@ package br.com.fiap.techChallenge3.usecase.review;
 
 import br.com.fiap.techChallenge3.entity.customer.gateway.CustomerGateway;
 import br.com.fiap.techChallenge3.entity.customer.model.Customer;
+import br.com.fiap.techChallenge3.entity.restaurant.exception.RestaurantNotFoundException;
 import br.com.fiap.techChallenge3.entity.restaurant.gateway.RestaurantGateway;
 import br.com.fiap.techChallenge3.entity.restaurant.model.Restaurant;
 import br.com.fiap.techChallenge3.entity.review.gateway.ReviewGateway;
@@ -15,6 +16,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -60,6 +62,24 @@ public class CreateReviewUseCaseTest {
         assertEquals(result.getRating(), 10.0);
         assertEquals(result.getComment(), "comentario");
         verify(reviewGateway, times(1)).create(any(Review.class));
+
+    }
+
+    @Test
+    public void deveCriarRestaurantSemSucessoTest() {
+
+        Restaurant restaurant = new Restaurant(
+                "The Best Of Recife", "Recife", "Brasileira", LocalTime.parse("19:00", DateTimeFormatter.ofPattern("HH:mm")), 50);
+        Customer customer = new Customer(
+                "augusto.junior", "augusto@junior.com", "123password", "augusto@junior.com", "46339814042", "1132659848");
+        IReviewRegistrationData registrationData = mock(IReviewRegistrationData.class);
+        Review review = new Review(restaurant, customer, 10.00, "comentario");
+        when(reviewGateway.create(any())).thenReturn(review);
+
+        assertThrows(RestaurantNotFoundException.class, () -> {
+            Review result = createReviewUseCase.execute(registrationData);
+        });
+        verify(reviewGateway, times(0)).create(any(Review.class));
 
     }
 
